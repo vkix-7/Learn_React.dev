a630b95a070d26c0407292c845d38ad1e0c07ff5
vkix_7 <vijayku123456789@gmail.com>
2024-08-03 00:13:13 +0530

    update

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -4,8 +4,8 @@ const Header = (props) => {
     return (<>
         
         <div class='responsiv-div'> <h1>This is Header.</h1>
-            <p>this is para for header.
-            </p>
+            The header element represents introductory content or a set of navigational aids within an HTML document.
+            It’s not the visible top portion of a web page (like the header you see on a website), but rather a container for metadata and essential information.
             <p>My name is {props.name}</p></div></>
     )
 }
